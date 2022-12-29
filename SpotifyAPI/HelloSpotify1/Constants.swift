d50f87585ee6f49e5570c74d730967b26feb2055
jrs1805 <jan.robin.scholz@googlemail.com>
2022-12-29 11:54:20 +0100

    jrs: UIKit SpotifyAPI is working on iPhone 8 with iOS 16.2

---
+++ SpotifyAPI/HelloSpotify1/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  Guessify
+//
+//  Created by code on 19.12.22.
+//
+
+import Foundation
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"guessify://")!
+let spotifyClientId = "d447887b24d94a829f0863cff20ddda5"
+let spotifyClientSecretKey = "548871f8b9d2409eab30d1541b1f841c"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
---
+++ SpotifyAPI/HelloSpotify1/foo.m
@@ -0,0 +1,8 @@
+//
+//  foo.m
+//  HelloSpotify1
+//
+//  Created by code on 28.12.22.
+//
+
+#import <Foundation/Foundation.h>
